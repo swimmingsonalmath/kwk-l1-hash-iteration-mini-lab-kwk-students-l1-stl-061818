287448e54d259843b01673c6131dac5f4af5e47e
Sonali Sharma <sonalisharma58521@gmail.com>
2018-07-05 21:41:03 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,27 +1,27 @@
-# def create_olympics_hash
-#   summer_olympics={
-#   :Sydney => "2000",
-#   :Athens => "2004",
-#   :Beijing => "2008",
-#   :London => "2012"}
-# summer_olympics[:Atlanta]="1996"
+ def create_olympics_hash
+   summer_olympics={
+   :Sydney => "2000",
+   :Athens => "2004",
+   :Beijing => "2008",
+   :London => "2012"}
+ summer_olympics[:Atlanta]="1996"
 
-# places = summer_olympics.keys
-# years = summer_olympics.values
+ places = summer_olympics.keys
+ years = summer_olympics.values
 
-# summer_olympics.each do |places, years|
-#     puts "The #{years} summer olympics took places in #{places}."
+summer_olympics.each do |places, years|
+     puts "The #{years} summer olympics took places in #{places}."
 
 
-# end
-# end
-# create_olympics_hash
+ end
+ end
+ create_olympics_hash
 
-# Implement this method so that it returns a hash with the data provided on README.md
-# def add_a_key_value_pair
+ Implement this method so that it returns a hash with the data provided on README.md
+ def add_a_key_value_pair
 
-# end 
-# add_a_key_value_pair
+ end 
+ add_a_key_value_pair
  
  
   
